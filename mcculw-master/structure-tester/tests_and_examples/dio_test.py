4b98672239d179111af86b30b79ca67e23183f8b
Kayden <eltrancy@gmail.com>
2022-02-16 10:46:11 -0500

    Working on DIO

--- mcculw-master/structure-tester/tests_and_examples/dio_test.py
+++ mcculw-master/structure-tester/tests_and_examples/dio_test.py
@@ -1,7 +1,16 @@
-from mcculw import ul
-from mcculw.enums import DigitalIODirection
-from mcculw.device_info import DaqDeviceInfo as DDI
-from mcculw.device_info import DaqiInfo
+from __future__ import absolute_import, division, print_function
+from builtins import *  # @UnusedWildImport
 
-ul.create_daq_device(0,     )
-ul.d_config_port()
+from mcculw import ul
+from mcculw.enums import DigitalIODirection as diod
+from mcculw.device_info import DaqDeviceInfo as ddi
+
+# configure board 0, port 10 (a0) to input
+ul.d_config_port(0, 10, diod.IN)
+# read input and assign it to x
+x = ul.d_bit_in(0, 10, 0)
+# print x
+print(x)
+
+ul.d_config_port(0, 22, diod.OUT)
+ul.d_out(0, 22, 1)
