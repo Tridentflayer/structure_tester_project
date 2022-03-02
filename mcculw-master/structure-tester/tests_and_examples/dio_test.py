33c1c3176935ed816272cc0a940f390043d20210
Kayden <eltrancy@gmail.com>
2022-03-02 10:31:15 -0500

    Input and Output FINALLY working!

--- mcculw-master/structure-tester/tests_and_examples/dio_test.py
+++ mcculw-master/structure-tester/tests_and_examples/dio_test.py
@@ -5,12 +5,37 @@ from mcculw import ul
 from mcculw.enums import DigitalIODirection as diod
 from mcculw.device_info import DaqDeviceInfo as ddi
 
-# configure board 0, port 10 (a0) to input
-ul.d_config_port(0, 10, diod.IN)
-# read input and assign it to x
-x = ul.d_bit_in(0, 10, 0)
-# print x
-print(x)
+def run_example():
+    # By default, the example detects and displays all available devices and
+    # selects the first device listed. Use the dev_id_list variable to filter
+    # detected devices by device ID (see UL documentation for device IDs).
+    # If use_device_detection is set to False, the board_num variable needs to
+    # match the desired board number configured with Instacal.
+    use_device_detection = True
+    dev_id_list = []
+    board_num = 0
 
-ul.d_config_port(0, 22, diod.OUT)
-ul.d_out(0, 22, 1)
+    daq_dev_info = ddi(board_num)
+    dio_info = ddi.get_dio_info(daq_dev_info)
+
+    port = next((port for port in dio_info.port_info if port.supports_output),
+            None)
+
+    if port.is_port_configurable:
+        ul.d_config_port(board_num, port.type, diod.OUT)
+
+    port_value = 0xFF
+    print('Setting', port.type.name, 'to', port_value)
+
+    # Output the value to the port
+    ul.d_out(board_num, port.type, port_value)
+
+    bit_num = 0
+    bit_value = 1
+    print('Setting', port.type.name, 'bit', bit_num, 'to', bit_value)
+
+    # Output the value to the bit
+    ul.d_bit_out(board_num, port.type, bit_num, bit_value)
+
+    if __name__ == '__main__':
+        run_example()
---
+++ mcculw-master/structure-tester/tests_and_examples/dio_test_2.py
@@ -0,0 +1,12 @@
+from mcculw import ul
+from mcculw.enums import DigitalIODirection as diod
+from mcculw.device_info import DaqDeviceInfo as ddi
+from mcculw.enums import DigitalPortType as dpt
+
+# Configure the A set of ports (0-7) as inputs
+ul.d_config_port(0, dpt.FIRSTPORTA, diod.IN)
+# Configure the B set of ports (8-15) as outputs
+ul.d_config_port(0, dpt.FIRSTPORTB, diod.OUT)
+
+# Output a 0 on port 8
+ul.d_bit_out(0, dpt.FIRSTPORTA, 8, 0)
