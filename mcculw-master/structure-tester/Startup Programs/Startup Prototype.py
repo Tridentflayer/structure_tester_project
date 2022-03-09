9644eaa569bfad746186fb6013b106af1237151b
ScholarlyStudent <scholarlystudent22@gmail.com>
2022-03-09 10:33:18 -0500

    Fixed cylinder bleed, added user friendly printout

--- mcculw-master/structure-tester/Startup Programs/Startup Prototype.py
+++ mcculw-master/structure-tester/Startup Programs/Startup Prototype.py
@@ -21,26 +21,15 @@ ManualLock = 1  # Create variable to toggle manual control. Lock is true by defa
 
 def cylinderbleed():
 
-    ventcontroller = 2
-
-    while ventcontroller == 1:
-        ul.d_out(0, Dpt.FIRSTPORTB, 8)
-        ul.d_out(0, Dpt.FIRSTPORTB, 9)
-
-
-    while ventcontroller > 1:
-        ul.d_out(0, Dpt.FIRSTPORTB, 8)
-        ul.d_out(0, Dpt.FIRSTPORTB, 9)
-        time.sleep(1)
-        ventcontroller = ventcontroller - 1
-        return ventcontroller
-
-
-
-
-
-
-
+    for ventcontroller in range(5, 0, -1):  # Cycle controller 5-0
+        ul.d_out(0, Dpt.FIRSTPORTB, 0)
+        ul.d_out(0, Dpt.FIRSTPORTB, 0)      # During this, send a signal to open the vent cylinders
+        time.sleep(.5)                  # Wait so the air has time to escape
+        if ventcontroller == 1:
+            ul.d_out(0, Dpt.FIRSTPORTB, 1)   # At 1, power the cylinders, so they're shut, and break the loop
+            ul.d_out(0, Dpt.FIRSTPORTB, 1)
+            break
+    return ventcontroller               # Send the variable out
 
 def magsensortest():
 
@@ -64,7 +53,7 @@ def pressuresensortest():
 
     pressuresensor = ul.a_in(1, 0, ULRange.BIP10VOLTS)  # Read pin to get analog value.
 
-    if pressuresensor == 2048:       # If the sensor reads zero volts, there is likely an error. Set to error state.
+    if pressuresensor == 2047:       # If the sensor reads zero volts, there is likely an error. Set to error state.
         pressuresensorstatus = 1
 
     else:
@@ -84,18 +73,49 @@ def distancesensortest():
 
     return distancesensorstatus    # Send distancesensorstatus out
 
+def lightbarcheck():
+    lightbarstatus = 1  # Once we get the light we'll figure out how to do this
+    return lightbarstatus
+
 # Split data from functions into usable components
 MagSensorStatus = (magsensortest())
 PressureSensorStatus = (pressuresensortest())
 DistanceSensorStatus = (distancesensortest())
-
+CylinderBleedStatus = (cylinderbleed())
+LightBarStatus = (lightbarcheck())
 # Temp Data Readout
 
-print("Mag Sensors =")
-print(MagSensorStatus)
-print("Pressure Sensor =")
-print(PressureSensorStatus)
-print("Distance Sensor =")
-print(DistanceSensorStatus)
-while 1 == 1:
-    print(cylinderbleed())
+if MagSensorStatus == 1 or PressureSensorStatus == 1 or DistanceSensorStatus == 1 or CylinderBleedStatus != 1 or LightBarStatus == 1:
+    ErrorState = 1  # Check for error state
+else:
+    ErrorState = 0
+
+if CylinderBleedStatus == 1:
+    print("Cylinder Bleed Successful!")
+else:
+    print("Cylinder Bleed Error!")
+
+if MagSensorStatus == 0:
+    print("Magnetic sensor check passed!")
+else:
+    print("Magnetic sensor check failed!")
+
+if PressureSensorStatus == 0:
+    print("Pressure sensor check passed!")
+else:
+    print("Pressure sensor check failed!")
+
+if DistanceSensorStatus == 0:
+    print("Distance sensor check passed!")
+else:
+    print("Distance sensor check failed!")
+
+if LightBarStatus == 0:
+    print("Light bar check passed!")
+else:
+    print("Light bar check failed!")
+
+if ErrorState == 0:
+    print("No error state!")
+else:
+    print("ERROR STATE!")
