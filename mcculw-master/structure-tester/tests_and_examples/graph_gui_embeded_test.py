ec20554b71c6f8d71842a3f4676fa721103e90bf
Kayden M Zada <eltrancy@gmail.com>
2022-03-28 10:08:39 -0400

    We need...parallel processing..............................................
    AAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAA

--- mcculw-master/structure-tester/tests_and_examples/graph_gui_embeded_test.py
+++ mcculw-master/structure-tester/tests_and_examples/graph_gui_embeded_test.py
@@ -1,136 +1,195 @@
-# This aint it chief
+from __future__ import unicode_literals
+from PyQt5 import QtCore, QtGui
+import os
+import time
 import sys
+from threading import *
+import random
 import numpy as np
 import matplotlib.pyplot as plt
-from matplotlib.backends.backend_qt5agg import FigureCanvasQTAgg as FigureCanvas
-from PyQt5.QtWidgets import QApplication, QWidget
-import itertools as itrt
-import matplotlib.animation as animation
 
-class Canvas(FigureCanvas):
-    def __init__(self, parent):
-        fig, self.ax = plt.subplots()
-        super().__init__(fig)
+from matplotlib.backends.backend_tkagg import FigureCanvasTkAgg as FigureCanvas
+
+progname = os.path.basename(sys.argv[0])
+progversion = "0.1"
+
+try:
+    _fromUtf8 = QtCore.QString.fromUtf8
+except AttributeError:
+    def _fromUtf8(s):
+        return s
+
+try:
+    _encoding = QtGui.QApplication.UnicodeUTF8
+    def _translate(context, text, disambig):
+        return QtGui.QApplication.translate(context, text, disambig, _encoding)
+except AttributeError:
+    def _translate(context, text, disambig):
+        return QtGui.QApplication.translate(context, text, disambig)
+
+class Ui_MainWindow(object):
+    def setupUi(self, MainWindow):
+        MainWindow.setObjectName(_fromUtf8("MainWindow"))
+        MainWindow.resize(800, 600)
+        self.centralwidget = QtGui.QWidget(MainWindow)
+        self.centralwidget.setObjectName(_fromUtf8("centralwidget"))
+        self.gridLayout_3 = QtGui.QGridLayout(self.centralwidget)
+        self.gridLayout_3.setObjectName(_fromUtf8("gridLayout_3"))
+        self.PropaanEquivalent = QtGui.QLabel(self.centralwidget)
+        self.PropaanEquivalent.setObjectName(_fromUtf8("PropaanEquivalent"))
+        self.gridLayout_3.addWidget(self.PropaanEquivalent, 4, 3, 1, 1)
+        self.CH4 = QtGui.QLabel(self.centralwidget)
+        self.CH4.setObjectName(_fromUtf8("CH4"))
+        self.gridLayout_3.addWidget(self.CH4, 2, 3, 1, 1)
+        self.CARI = QtGui.QLabel(self.centralwidget)
+        self.CARI.setObjectName(_fromUtf8("CARI"))
+        self.gridLayout_3.addWidget(self.CARI, 3, 0, 1, 1)
+        self.WobbeIndex = QtGui.QLabel(self.centralwidget)
+        self.WobbeIndex.setObjectName(_fromUtf8("WobbeIndex"))
+        self.gridLayout_3.addWidget(self.WobbeIndex, 2, 0, 1, 1)
+        self.WobbeIndexField = QtGui.QLineEdit(self.centralwidget)
+        self.WobbeIndexField.setObjectName(_fromUtf8("WobbeIndexField"))
+        self.gridLayout_3.addWidget(self.WobbeIndexField, 2, 1, 1, 1)
+        self.pushButton = QtGui.QPushButton(self.centralwidget)
+        self.pushButton.setObjectName(_fromUtf8("pushButton"))
+        self.gridLayout_3.addWidget(self.pushButton, 1, 0, 1, 1)
+        self.pushButton.clicked.connect(self.timer)
+        self.CARI_Field = QtGui.QLineEdit(self.centralwidget)
+        self.CARI_Field.setObjectName(_fromUtf8("CARI_Field"))
+        self.gridLayout_3.addWidget(self.CARI_Field, 3, 1, 1, 1)
+        self.CH4_field = QtGui.QLineEdit(self.centralwidget)
+        self.CH4_field.setObjectName(_fromUtf8("CH4_field"))
+        self.gridLayout_3.addWidget(self.CH4_field, 2, 4, 1, 1)
+        self.Propaan_field = QtGui.QLineEdit(self.centralwidget)
+        self.Propaan_field.setObjectName(_fromUtf8("Propaan_field"))
+        self.gridLayout_3.addWidget(self.Propaan_field, 4, 4, 1, 1)
+        self.Dichtheid = QtGui.QLabel(self.centralwidget)
+        self.Dichtheid.setObjectName(_fromUtf8("Dichtheid"))
+        self.gridLayout_3.addWidget(self.Dichtheid, 3, 3, 1, 1)
+        self.Verbrandingswaarde = QtGui.QLabel(self.centralwidget)
+        self.Verbrandingswaarde.setObjectName(_fromUtf8("Verbrandingswaarde"))
+        self.gridLayout_3.addWidget(self.Verbrandingswaarde, 4, 0, 1, 1)
+        self.Verbrandingswaarde_field = QtGui.QLineEdit(self.centralwidget)
+        self.Verbrandingswaarde_field.setObjectName(_fromUtf8("Verbrandingswaarde_field"))
+        self.gridLayout_3.addWidget(self.Verbrandingswaarde_field, 4, 1, 1, 1)
+        self.Dichtheid_field = QtGui.QLineEdit(self.centralwidget)
+        self.Dichtheid_field.setObjectName(_fromUtf8("Dichtheid_field"))
+        self.gridLayout_3.addWidget(self.Dichtheid_field, 3, 4, 1, 1)
+        spacerItem = QtGui.QSpacerItem(20, 40, QtGui.QSizePolicy.Minimum, QtGui.QSizePolicy.Expanding)
+        self.gridLayout_3.addItem(spacerItem, 5, 2, 1, 1)
+        self.result_field = QtGui.QLineEdit(self.centralwidget)
+        self.result_field.setObjectName(_fromUtf8("result_field"))
+        self.gridLayout_3.addWidget(self.result_field, 1, 1, 1, 1)
+        self.aantalSensoren_label = QtGui.QLabel(self.centralwidget)
+        self.aantalSensoren_label.setObjectName(_fromUtf8("aantalSensoren_label"))
+        self.gridLayout_3.addWidget(self.aantalSensoren_label, 1, 3, 1, 1)
+        self.aantalSensoren_field = QtGui.QLineEdit(self.centralwidget)
+        self.aantalSensoren_field.setObjectName(_fromUtf8("aantalSensoren_field"))
+        self.gridLayout_3.addWidget(self.aantalSensoren_field, 1, 4, 1, 1)
+        MainWindow.setCentralWidget(self.centralwidget)
+        self.menubar = QtGui.QMenuBar(MainWindow)
+        self.menubar.setGeometry(QtCore.QRect(0, 0, 656, 21))
+        self.menubar.setObjectName(_fromUtf8("menubar"))
+        MainWindow.setMenuBar(self.menubar)
+        self.statusbar = QtGui.QStatusBar(MainWindow)
+        self.statusbar.setObjectName(_fromUtf8("statusbar"))
+        MainWindow.setStatusBar(self.statusbar)
+
+        self.dc = MyMplCanvas(MainWindow, width=1, height=2, dpi=100)
+        self.gridLayout_3.addWidget(self.dc, 5, 0, 1, 5)
+
+        self.retranslateUi(MainWindow)
+        QtCore.QMetaObject.connectSlotsByName(MainWindow)
+
+    def retranslateUi(self, MainWindow):
+        MainWindow.setWindowTitle(_translate("MainWindow", "MainWindow", None))
+        self.PropaanEquivalent.setText(_translate("MainWindow", "Propaan Equivalent:", None))
+        self.CH4.setText(_translate("MainWindow", "CH4:", None))
+        self.CARI.setText(_translate("MainWindow", "CARI:", None))
+        self.WobbeIndex.setText(_translate("MainWindow", "Wobbe Index:", None))
+        self.pushButton.setText(_translate("MainWindow", "Start", None))
+        self.Dichtheid.setText(_translate("MainWindow", "Dichtheid:", None))
+        self.Verbrandingswaarde.setText(_translate("MainWindow", "Verbrandingswaarde:", None))
+        self.aantalSensoren_label.setText(_translate("MainWindow", "aantal aangesloten sensoren:", None))
+
+    def closeEvent(self, event):
+        reply = QtGui.QMessageBox.question(self, 'Message',
+                                           "Are you sure to quit?", QtGui.QMessageBox.Yes |
+                                           QtGui.QMessageBox.No, QtGui.QMessageBox.No)
+
+        if reply == QtGui.QMessageBox.Yes:
+            event.accept()
+        else:
+            event.ignore()
+        self.fileQuit()
+
+    def task(self):
+        while True:
+            ui.dc.update_figure()
+            time.sleep(1.0)
+
+    def timer(self):
+        t = Timer(1.0, self.task())
+        t.start()
+
+def getCO22():
+    '''return the CO2 concentration in the atmosphere
+    '''
+    return 369.56 + random.random()*50
+
+
+class MyMplCanvas(FigureCanvas):
+    """Ultimately, this is a QWidget (as well as a FigureCanvasAgg, etc.)."""
+    def __init__(self, parent=None, width=5, height=4, dpi=100):
+        self.fig = plt.figure(figsize=(width, height), dpi=dpi)
+        # self.fig = Figure(figsize=(width, height), dpi=dpi)
+        super(MyMplCanvas,self).__init__(self.fig)
+        self.yAxe = np.array([0])
+        self.xAxe = np.array([0])
+        self.i = 0
+
+        self.axes = self.fig.add_subplot(111)
+        self.axes.autoscale(False)
+        #We want the axes cleared every time plot() is called
+        self.axes.hold(False)
+        self.axes.set_ylim(0, 500)
+
+        self.compute_initial_figure()
+        # plt.show(block=False)
+
+        FigureCanvas.__init__(self, self.fig)
         self.setParent(parent)
 
-        # creates figure "ax" with a grid
-        self.ax.grid()
+        FigureCanvas.setSizePolicy(self,
+                                   QtGui.QSizePolicy.Expanding,
+                                   QtGui.QSizePolicy.Expanding)
+        FigureCanvas.updateGeometry(self)
 
-        # creates time
-        t = []
+    def compute_initial_figure(self):
+        self.axes.plot([0, 1, 2, 3, 4, 5, 6, 7, 8, 9], [0, 0, 0, 0, 0, 0, 0, 0, 0, 0])
 
-        # sets time as xdata
-        xdata = t
+    def update_figure(self):
+        self.yAxe = np.append(self.yAxe, (getCO22()))
+        self.xAxe = np.append(self.xAxe, self.i)
+        print(self.xAxe, self.yAxe)
+        if len(self.yAxe) > 10:
+            self.yAxe = np.delete(self.yAxe, 0)
 
-        # ydata inputs
-        ydataps1 = []
-        ydataps2 = []
-        ydataps3 = []
-        ydataps4 = []
-        ydatads1 = []
+        if len(self.xAxe) > 10:
+            self.xAxe = np.delete(self.xAxe, 0)
 
-        # generates data for the graph
-        def datagen():
-            # creates timer on the x-axis
-            for cnt in itrt.count():
-                t = cnt / 10
-                # y value inputs
-                ps1y = np.log(np.pi * t)
-                ps2y = np.log(5 * np.pi * t)
-                ps3y = np.log(3 * np.pi * t)
-                ps4y = np.log(2 * np.pi * t)
-                ds1y = np.log(1.5 * np.pi * t)
+        self.axes.plot(self.xAxe, self.yAxe)
+        self.axes.set_xlim(self.xAxe[0],self.xAxe[len(self.xAxe)-1])
+        self.axes.grid(True)
 
-                # yields data to move it to run function
-                yield t, ps1y, ps2y, ps3y, ps4y, ds1y
+        self.i = self.i + 1
 
-        # # creates lines with line-width two, at points t and y[]
-        lineps1, = self.ax.plot(t, [], lw=2)
-        lineps2, = self.ax.plot(t, [], lw=2)
-        lineps3, = self.ax.plot(t, [], lw=2)
-        lineps4, = self.ax.plot(t, [], lw=2)
-        lineds1, = self.ax.plot(t, [], lw=2)
+        self.fig.canvas.draw()
 
-        # init function, clears line data and sets the line data to be t and y[]
-        def init():
-            # clear data
-            del xdata[:]
-            del ydataps1[:]
-            del ydataps2[:]
-            del ydataps3[:]
-            del ydataps4[:]
-            del ydatads1[:]
-
-
-            # set line data to cleared
-            lineps1.set_data(xdata, ydataps1)
-            lineps2.set_data(xdata, ydataps2)
-            lineps3.set_data(xdata, ydataps3)
-            lineps4.set_data(xdata, ydataps4)
-            lineds1.set_data(xdata, ydatads1)
-
-
-            # return updated values
-            return lineps1, lineps2, lineps3, lineps4, lineds1
-
-        # updates values for data
-        def run(data):
-            # time (t) and y = data
-            t, yps1, yps2, yps3, yps4, yds1 = data
-            # update x to be set to time
-            xdata.append(t)
-            # update ydatas to new y values
-            ydataps1.append(yps1)
-            ydataps2.append(yps2)
-            ydataps3.append(yps3)
-            ydataps4.append(yps4)
-            ydatads1.append(yds1)
-
-
-            # auto-scaling (kinda)
-            xmin, xmax = self.ax.get_xlim()
-            ymin, ymax = self.ax.get_ylim()
-
-            # compares all y data to ensure the graph scales on the highest value
-            ydata_list = [yps1, yps2, yps3, yps4, yds1]
-            max_value = max(ydata_list)
-
-            # y scale
-            if max_value >= ymax:
-                self.ax.set_ylim(ymin, 2*ymax)
-                self.ax.figure.canvas.draw()
-
-            # Time autoscale
-            if t >= xmax:
-                self.ax.set_xlim(xmin, 2*xmax)
-                self.ax.figure.canvas.draw()
-
-            # updates lines
-            lineps1.set_data(xdata, ydataps1)
-            lineps2.set_data(xdata, ydataps2)
-            lineps3.set_data(xdata, ydataps3)
-            lineps4.set_data(xdata, ydataps4)
-            lineds1.set_data(xdata, ydatads1)
-
-
-            # returns updated line values
-            return lineps1, lineps2, lineps3, lineps4, lineds1
-
-        # creates an animation function which runs all the functions in a loop
-        ani = animation.FuncAnimation(fig, run, datagen, interval=1, init_func=init)
-
-        # show graph, absolutely necessary
-        plt.show(block=True)
-
-
-class AppDemo(QWidget)      :
-    def __init__(self):
-        super().__init__()
-        self.resize(1600, 800)
-
-        chart = Canvas(self)
-
-app = QApplication(sys.argv)
-demo = AppDemo()
-demo.show()
+# if  __name__ =='__main__':
+app = QtGui.QApplication(sys.argv)
+MainWindow = QtGui.QMainWindow()
+ui = Ui_MainWindow()
+ui.setupUi(MainWindow)
+MainWindow.show()
 sys.exit(app.exec_())
