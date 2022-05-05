619d295f03f454771b13b10434cb335c05b71b3d
ScholarlyStudent <scholarlystudent22@gmail.com>
2022-05-05 10:22:06 -0400

    files

--- mcculw-master/structure-tester/Startup Programs/Data Processing.py
+++ mcculw-master/structure-tester/Startup Programs/Data Processing.py
@@ -17,10 +17,11 @@ DataProcessing = 1   # Controls Data processing
                                                     # Get current date and time, create and open a file with that name
 x = datetime.now()
 file_name = x.strftime('%d-%m-%Y-%H-%M-%S.txt')
-with open(file_name, 'w') as file:
-    print('created', file_name)     # Open the file, and print column headers
-L1 = ["Load Cell 1, ", "Load Cell 2, ", "Load Cell 3, ", "Load Cell 4, ", "Distance Sensor"]
-
+file = open(file_name, 'w')
+print('created', file_name)     # Open the file, and print column headers
+file.writelines("Load Cell 1, Load Cell 2, Load Cell 3, Load Cell 4, Distance Sensor")
+file.writelines("\n")
+file.writelines("\n")
 while DataProcessing == 1:      # Loop for processing data while active
 
     LoadCell1Raw = ul.a_in(1, 1, ULRange.BIP10VOLTS)         # Channel 1, Port 4
@@ -29,19 +30,23 @@ while DataProcessing == 1:      # Loop for processing data while active
     LoadCellBRaw = ul.a_in(1, 10, ULRange.BIP10VOLTS)        # Channel 10, Port 8   Taking raw sensor data
     DistanceSensorRaw = ul.a_in(1, 3, ULRange.BIP10VOLTS)    # Channel 3, Port 10
 
-    LoadCell1Final = LoadCell1Raw * 10.8
-    LoadCell2Final = LoadCell2Raw * 12.6     # Multiplying the raw data by the experimentally determined constants
-    LoadCell3Final = LoadCell3Raw * 12       # Gives us the actual forces/distances
-    LoadCellBFinal = LoadCellBRaw * 113
-    DistanceSensorFinal = (DistanceSensorRaw**-1) * 10.52631579  # Using the inverse to calculate the approximate value.
-                                                                # Approx. 0.05% error on distance sensor
+    LoadCell1Mid = int(LoadCell1Raw * 10.8 * 1000)
+    LoadCell2Mid = int(LoadCell2Raw * 12.6 * 1000)  # Multiplying the raw data by the constants
+    LoadCell3Mid = int(LoadCell3Raw * 12 * 1000)    # Gives us the actual forces/distances
+    LoadCellBMid = int(LoadCellBRaw * 113 * 1000)
+    DistanceSensorMid = int((DistanceSensorRaw**-1) * 10.52631579 * 1000)
 
-    L2 = [LoadCell1Final, LoadCell2Final, LoadCell3Final, LoadCellBFinal, DistanceSensorFinal]
+    LoadCell1Final = str((float(LoadCell1Mid)/1000))
+    LoadCell2Final = str((float(LoadCell2Mid)/1000))
+    LoadCell3Final = str((float(LoadCell3Mid)/1000))
+    LoadCellBFinal = str((float(LoadCellBMid)/1000))
+    DistanceSensorFinal = str((float(DistanceSensorMid)/1000))
+
+    L2 = [LoadCell1Final, "      ", LoadCell2Final, "      ", LoadCell3Final, "      ", LoadCellBFinal, "      "]
+    L3 = [DistanceSensorFinal]
     file.writelines(L2)         # Writing the data to the file
-
-    if DistanceSensorFinal == 1:     # If deflection exceed te max, close the file
-        file.close()
-
+    file.writelines(L3)
+    file.writelines("\n")
     time.sleep(0.1)     # Time delay. Can be modified to change the resolution of the data
 
 if DataProcessing != 1:
