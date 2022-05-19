166e0329f9fd60f676a0eff798b1f226a75190e9
ScholarlyStudent <scholarlystudent22@gmail.com>
2022-05-19 09:45:05 -0400

    Progress!

--- mcculw-master/structure-tester/Startup Programs/Data Processing.py
+++ mcculw-master/structure-tester/Startup Programs/Data Processing.py
@@ -19,7 +19,7 @@ file.writelines("\n")
 file.writelines("\n")
 
 DataProcessing = 0
-TareAvg = 20
+TareAvg = 50
 MathController = 0         # Math related variables
 Tare1Total = 0
 Tare2Total = 0
@@ -32,7 +32,7 @@ Tare3 = 0                  # More of the same
 TareB = 0
 DTare = 0
 
-for TareAvg in range(50, 1, -1):   # Main loop for calculating the tare value
+for TareAvg in range(51, 1, -1):   # Main loop for calculating the tare value
 
     LoadCell1Tare = ul.a_in(1, 1, ULRange.BIP10VOLTS)
     LoadCell2Tare = ul.a_in(1, 9, ULRange.BIP10VOLTS)
@@ -45,13 +45,14 @@ for TareAvg in range(50, 1, -1):   # Main loop for calculating the tare value
     Tare3Total = Tare3Total + LoadCell3Tare   # Continuously add inputs
     TareBTotal = TareBTotal + LoadCellBTare
     DSTotal = DSTotal + DSTareRaw
-    time.sleep(.1)      # Delay. Change to increase tare resolution
+    time.sleep(.1)      # Delay. Change to increase tare resolution.
 
 Tare1 = Tare1Total/50
 Tare2 = Tare2Total/50
 Tare3 = Tare3Total/50       # Take the total divide by the number of cycles
 TareB = TareBTotal/50
 DTare = DSTotal/50
+print(Tare1, Tare2, Tare3, TareB)
 DataProcessing = 1   # Then activate the recording section
 print("Tare Found. Collecting data")
 
@@ -66,7 +67,7 @@ while DataProcessing == 1:      # Loop for processing data while active
     LoadCell1Mid = int(LoadCell1Raw * 0.000305 * 10.8 * 100)  # 1-10.8, 2-12.6, 3-12, B-10.8
     LoadCell2Mid = int(LoadCell2Raw * 0.000305 * 12.6 * 100)  # Multiplying the raw data by the constants
     LoadCell3Mid = int(LoadCell3Raw * 0.000305 * 12 * 100)    # Gives us the actual forces/distances
-    LoadCellBMid = int(LoadCellBRaw * 0.000305 * 10.8 * 100)  # Need to change 10.8, 12.6, 12, and 10.8.
+    LoadCellBMid = int(LoadCellBRaw * 0.000305 * 9.2 * 100)  # Need to change 10.8, 12.6, 12, and 10.8.
     DistanceSensorMid = int((DistanceSensorRaw**-1) * 10.52631579 * 100)
 
     LoadCell1Final = str((float(LoadCell1Mid)/100))
@@ -74,10 +75,11 @@ while DataProcessing == 1:      # Loop for processing data while active
     LoadCell3Final = str((float(LoadCell3Mid)/100))      # Continuing the math
     LoadCellBFinal = str((float(LoadCellBMid)/100))
     DistanceSensorFinal = str((float(DistanceSensorMid)/100))
-    TotalLoad = str(int(float(LoadCell1Mid/100) + float(LoadCell2Mid/100) + float(LoadCell3Mid/100) + float(LoadCellBMid/100)))
+    TotalLoad1 = float((LoadCell1Mid + LoadCell2Mid + LoadCell3Mid + LoadCellBMid)/100)
+    TotalLoad2 = str(TotalLoad1)
 
     L2 = [LoadCell1Final, "kg   /  ", LoadCell2Final, "kg   /  ", LoadCell3Final, "kg   /  "]
-    L3 = [LoadCellBFinal, "kg   /  ~", TotalLoad, "kg   /  ", DistanceSensorFinal, "in"]
+    L3 = [LoadCellBFinal, "kg   /  ", TotalLoad2, "kg   /  ", DistanceSensorFinal, "in"]
     file.writelines(L2)         # Writing the data to the file
     file.writelines(L3)
     file.writelines("\n")
